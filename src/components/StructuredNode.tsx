6d1646e0756159138ad39ca317f2021054700813
Atte Liimatainen <atte.liimat@gmail.com>
2023-03-21 14:32:03 +0200

    fix logic
    - parse charcode first
    - add todo for later

--- src/components/StructuredNode.tsx
+++ src/components/StructuredNode.tsx
@@ -9,16 +9,12 @@ export const StructuredNode = (props: Props) => {
   const { bg, fg, length, Text, charcode } = props.node
 
   let text = "";
-  if (Text) {
+  if (charcode) {
+    // TODO: figure out how charcode works
+    text = "\u00a0".repeat(Number(length))
+  } else if (Text) {
     // replace spaces with no-break spaces
     text = Text.replace(/ /g, "\u00A0")
-  } else if (charcode) {
-    // charcodes might have h-suffix
-    let c = charcode.at(-1) === "h"
-      ? charcode.substring(0, charcode.length - 1)
-      : charcode
-
-    text = c.toString()
   } else {
     text = "\u00a0".repeat(Number(length))
   }
